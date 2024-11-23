8445db550987bc9336d2b532dc257fe088d8dbb1
Pedro Pinho <ptfpinho@hotmail.com>
2024-11-23 10:44:38 +0000

    update benchmarking setup

--- benchmarks/test.c
+++ benchmarks/test.c
@@ -3,6 +3,13 @@
 int main() {
     // Perform calculations
     long long x = 10 / 2;
+    long long d;
+    if (x == 4) {
+        d = 50 + 3;
+    } else {
+        d = 50 - 1;
+    }
+    printf("%lld\n", d);
     // Print results
     printf("%lld\n", x);
 
--- benchmarks/test.py
+++ benchmarks/test.py
@@ -1,5 +1,12 @@
 def main():
     x = 10 / 2
     print(x)
+
+    if x == 4:
+        d = 50 + 3
+        print(d)
+    else:
+        d = 50 - 1
+        print(d)
 if __name__ == "__main__":
     main()
